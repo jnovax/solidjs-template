fde61220a04b6df28735f3cd7d86e4b0410af424
Lam Nguyen <hlam385@gmail.com>
2023-07-16 22:19:47 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+const colors = require('tailwindcss/colors')
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./node_modules/flowbite/**/*.js"
+  ],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        primary: colors.blue,
+        secondary: colors.cyan
+      }
+    }
+  },
+  plugins: [
+    require("@kobalte/tailwindcss"),
+    require('flowbite/plugin')
+  ],
+}
+
